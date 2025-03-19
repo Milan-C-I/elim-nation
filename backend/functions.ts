ff2bc3b67900b29aa89e4406a08a9713d080233c
jtuluve <jtuluve@gmail.com>
2025-03-19 21:24:01 +0530

    Backend models and methods added

---
+++ backend/functions.ts
@@ -0,0 +1,49 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+export async function getUsers() {
+    return await db.user.findMany();
+}
+
+export async function getUser(id: number) {
+    return await db.user.findUnique({
+        where: {
+            id: id
+        }
+    });
+}
+
+export async function createUser({name,blank,checked,eliminated}:Prisma.UserCreateInput) {
+    return await db.user.create({
+        data: {
+            name,
+            blank,
+            checked,
+            eliminated
+        }
+    });
+}
+
+export async function updateUser(id:number, data:Prisma.UserUpdateInput){
+    return await db.user.update({
+        where: {
+            id: id
+        },
+        data: data
+    })
+}
+
+export async function updateUsers(data:Prisma.UserUncheckedUpdateManyInput){
+    return await db.user.updateMany({
+        data: data
+    })
+}
+
+export async function deleteUser(id:number){
+    return await db.user.delete({
+        where: {
+            id: id
+        }
+    })
+}
